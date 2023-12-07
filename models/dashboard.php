ba801490e98d2f21d883899c75f8994d7c0fcd94
Hao Pham-Duc <hao.pham1652003@gmail.com>
2023-12-07 20:50:18 +0700

    UI cho Dashboard

--- models/dashboard.php
+++ models/dashboard.php
@@ -1,2 +1,40 @@
 <?php
 require_once('connection.php');
+
+function get_total_building() {
+    $db = DB::getInstance();
+    // $req = $db->query();
+    return "# of Buildings...";
+}
+
+function get_total_room() {
+    $db = DB::getInstance();
+    // $req = $db->query();
+    return "# of Rooms...";
+}
+
+function get_total_slot() {
+    $db = DB::getInstance();
+    // $req = $db->query();
+    return "# of Slots...";
+}
+
+function get_total_student_still_lives() {
+    $db = DB::getInstance();
+    // $req = $db->query();
+    return "# of Students...";
+}
+
+function get_total_employee() {
+    $db = DB::getInstance();
+    // $req = $db->query();
+    return "# of Employees...";
+}
+
+function get_total_manager() {
+    $db = DB::getInstance();
+    // $req = $db->query();
+    return "# of Managers...";
+}
+
+
