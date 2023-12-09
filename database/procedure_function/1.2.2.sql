99d93f6e0e457a48c00666d481fb7fce36fe3d25
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2023-12-10 00:06:56 +0700

    update 1.2.1, add 1.2.2

--- database/procedure_function/1.2.2.sql
+++ database/procedure_function/1.2.2.sql
@@ -1,13 +1,73 @@
--- DELIMITER //
--- USE chetcom //
--- DROP TRIGGER UpdateStudentCount;
--- CREATE TRIGGER UpdateStudentCount
---     AFTER DELETE
---     ON STUDENT
---     FOR EACH ROW
--- BEGIN
-    
--- END //
--- DELIMITER ;
--- SET FOREIGN_KEY_CHECKS = 0;
--- DELETE FROM STUDENT WHERE CCCD_number = 025303001515;
+ALTER TABLE ROOM ADD COLUMN student_count INT;
+
+DELIMITER //
+USE chetcom //
+CREATE TRIGGER UpdateStudentCount
+    AFTER INSERT
+    ON LIVES_IN
+    FOR EACH ROW
+BEGIN
+    DECLARE v_student_count INT;
+    DECLARE v_room_id VARCHAR(5);
+
+    SET v_room_id = NEW.Room_ID;
+
+    SET v_student_count = (
+        SELECT COUNT(*)
+        FROM LIVES_IN
+        WHERE Room_ID = v_room_id
+    );
+
+    UPDATE ROOM
+    SET status = CASE
+            WHEN v_student_count > 0 THEN 'A'
+            ELSE 'F'
+        END,
+        student_count = v_student_count
+    WHERE Room_ID = v_room_id;
+END //
+
+CREATE TRIGGER UpdateStudentCount
+    AFTER DELETE
+    ON LIVES_IN
+    FOR EACH ROW
+BEGIN
+    DECLARE v_student_count INT;
+    DECLARE v_room_id VARCHAR(5);
+
+    SET v_room_id = OLD.Room_ID;
+
+    SET v_student_count = (
+        SELECT COUNT(*)
+        FROM LIVES_IN
+        WHERE Room_ID = v_room_id
+    );
+
+    UPDATE ROOM
+    SET status = CASE
+            WHEN v_student_count > 0 THEN 'A'
+            ELSE 'F'
+        END,
+        student_count = v_student_count
+    WHERE Room_ID = v_room_id;
+END //
+
+CREATE TRIGGER UpdateStudentCount
+    AFTER UPDATE
+    ON LIVES_IN
+    FOR EACH ROW
+BEGIN
+    DECLARE v_student_count INT;
+    DECLARE v_room_id VARCHAR(5);
+
+    SET v_room_id = NEW.Room_ID;
+
+    UPDATE ROOM
+    SET status = CASE
+            WHEN v_student_count > 0 THEN 'A'
+            ELSE 'F'
+        END,
+        student_count = v_student_count
+    WHERE Room_ID = v_room_id;
+END //
+DELIMITER ;
