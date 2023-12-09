c6395613df4aa703066f8775b50c21aea25aeb67
Nguyễn Thái Tân <nguyenthaitan02@gmail.com>
2023-12-09 22:43:20 +0700

    * fix bug student view

--- public/js/student/index.js
+++ public/js/student/index.js
@@ -1,25 +1,24 @@
-// $("#tab-student").DataTable({
-//     responsive: true,
-//     lengthChange: false,
-//     autoWidth: false,
-//     language: {
-//         url: "//cdn.datatables.net/plug-ins/1.10.25/i18n/Vietnamese.json",
-//     },
-//     initComplete: function () {
-//         this.api().columns().every(function () {
-//             var column = this;
-//             $('<input type="text" placeholder="Tìm kiếm"/>')
-//                 .css({'width': '120px', 'margin-bottom': '10px', 'margin-top': '10px', 'text-align': 'center'})
-//                 .appendTo($(column.header()))
-//                 .on('keyup change click', function (e) {
-//                     e.stopPropagation();
-//                     column.search($(this).val(), false, false, true).draw();
-//                 });
-//         });
-//     },
-// 	lengthMenu: [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
-//     pageLength: 5
-// });
+$("#tab-student").DataTable({
+    responsive: false,
+    lengthChange: false,
+    autoWidth: false,
+    language: {
+        url: "//cdn.datatables.net/plug-ins/1.10.25/i18n/Vietnamese.json",
+    },
+    // initComplete: function () {
+    //     this.api().columns().every(function () {
+    //         var column = this;
+    //         $('<input type="text" placeholder="Tìm kiếm"/>')
+    //             .css({'width': '120px', 'margin-bottom': '10px', 'margin-top': '10px', 'text-align': 'center'})
+    //             .appendTo($(column.header()))
+    //             .on('keyup change click', function (e) {
+    //                 e.stopPropagation();
+    //                 column.search($(this).val(), false, false, true).draw();
+    //             });
+    //     });
+    // },
+    pageLength: 10
+});
 
 $(".btn-edit").click(function (e) {
     var id = $(this).data("id");
